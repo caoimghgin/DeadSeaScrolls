54384a082530dc78773f41d81c304611c2eb4228
Kevin Muldoon <caoimghgin@gmail.com>
2014-07-22 09:04:23 -0400

    Initial Commit

---
+++ DeadSeaScrolls/RJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RJAppDelegate.h
+//  DeadSeaScrolls
+//
+//  Created by Kevin Muldoon on 7/22/14.
+//  Copyright (c) 2014 RobotJackalope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DeadSeaScrolls/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DeadSeaScrolls
+//
+//  Created by Kevin Muldoon on 7/22/14.
+//  Copyright (c) 2014 RobotJackalope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RJAppDelegate class]));
+    }
+}
